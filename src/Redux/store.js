8a4d2f848d4bc9ad56985104bb757fb0f6b3f499
Sevenpros <sethbisoso@gmail.com>
2022-12-05 10:52:46 +0100

    Fix linters errors

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-// import greetingReducer from './greeting';
 
 const store = configureStore({
   reducer: {
-    // greetingReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
