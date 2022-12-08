59ab3c96d9dd11528401e256b0b9ab7dbc58046d
eaesmat <ea.ahmadzai2020@gmail.com>
2022-12-08 13:16:18 +0430

    Add table for doctor data

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -13,7 +13,36 @@ const Details = () => {
   }, []);
   return (
     <div className="container d-flex justify-content-center align-items-center">
-      <h1 className="fw-bolder">{doctor.last_name}</h1>
+      <div>
+        <img src={doctor.photo} alt="Doctor-Img" className="doctor-img" />
+      </div>
+      <div className="container d-flex justify-content-center align-items-center">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name </th>
+              <th scope="col">City</th>
+              <th scope="col">Contact Number</th>
+              <th scope="col">E-mail</th>
+              <th scope="col">Bio</th>
+              <th scope="col">Specialization</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">{doctor.first_name}</th>
+              <td>{doctor.last_name}</td>
+              <td>{doctor.city}</td>
+              <td>{doctor.phone}</td>
+              <td>{doctor.email}</td>
+              <td>{doctor.bio}</td>
+              <td>{doctor.specialization}</td>
+
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
