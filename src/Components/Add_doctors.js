7bf8e46ffd5edc72dcf84ad35a1c82f8d61cb6ea
Isigba Ambrose <ambrisexyz@gmail.com>
2022-12-12 13:08:55 +0100

    added validation add-doctor input field

--- src/Components/Add_doctors.js
+++ src/Components/Add_doctors.js
@@ -3,8 +3,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-
+import { ToastContainer, toast} from 'react-toastify';
 import { addDoctor } from '../Redux/doctors/doctor';
 
 const AddDoctor = () => {
@@ -21,16 +20,20 @@ const AddDoctor = () => {
     
     const onClickHandle = (e) => {
         e.preventDefault();
-        toast.success("You have succesfully booked this Doctor", {
-            position: "top-center",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-          });
+
+        const validate = (doctor) => {
+            return (
+                doctor.first_name === '' ||
+                doctor.last_name === '' ||
+                doctor.city === '' ||
+                doctor.phone === '' ||
+                doctor.email === '' ||
+                doctor.bio === '' ||
+                doctor.specialization === '' ||
+                doctor.photo === ''
+            );
+        };
+
         const doctor = {};
         doctor.first_name = first_name;
         doctor.last_name = last_name;
@@ -40,32 +43,31 @@ const AddDoctor = () => {
         doctor.bio = bio;
         doctor.specialization = specialization;
         doctor.photo = photo;
+
+           if (validate(doctor)) {
+            toast.error('All fields must be filled !', {
+                position: toast.POSITION.TOP_CENTER,
+            });
+            return;
+        }
+    
         dispatch(addDoctor(doctor));
     };
     
     return (
-        <div className="container d-flex justify-content-center align-items-center">
+        <div className="container-fluid d-flex justify-content-center align-items-center w-100 ml-4">
         <div className="container d-grid justify-content-center align-items-center">
-            <table className="table d-flex grid-template-column-1fr 1fr">
-            <thead>
-                <tr className="card d-grid">
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name </th>
-                <th scope="col">City</th>
-                <th scope="col">Contact Number</th>
-                <th scope="col">E-mail</th>
-                <th scope="col">Bio</th>
-                <th scope="col">Specialization</th>
-                <th scope="col">Photo</th>
-                </tr>
-            </thead>
+            <table className="table borderless d-flex grid-template-column-1fr 1fr w-70">
+           
             <tbody>
-                <tr className="card d-grid">
-                <th scope="row">
+                <tr className="card d-grid w-100 mr-0">
+                <th scope="row borderless">
                     <input
                     type="text"
                     value={first_name}
                     onChange={(e) => setFirstName(e.target.value)}
+                    className="form-control borderless"
+                    placeholder='Enter first name'
                     />
                 </th>
                 <td>
@@ -73,6 +75,8 @@ const AddDoctor = () => {
                     type="text"
                     value={last_name}
                     onChange={(e) => setLastName(e.target.value)}
+                    className="form-control border-none"
+                    placeholder='Enter last name'
                     />
                 </td>
                 <td>
@@ -80,6 +84,8 @@ const AddDoctor = () => {
                     type="text"
                     value={city}
                     onChange={(e) => setCity(e.target.value)}
+                    className="form-control border-none"
+                    placeholder='Enter city name'
                     />
                 </td>
                 <td>
@@ -87,6 +93,8 @@ const AddDoctor = () => {
                     type="text"
                     value={phone}
                     onChange={(e) => setPhone(e.target.value)}
+                    className="form-control border-none "
+                    placeholder='Enter 1...10 digit number'
                     />
                 </td>
                 <td>
@@ -94,6 +102,8 @@ const AddDoctor = () => {
                     type="text"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
+                    className="form-control border-none"
+                    placeholder='Enter email'
                     />
                 </td>
                 <td>
@@ -101,6 +111,8 @@ const AddDoctor = () => {
                     type="text"
                     value={bio}
                     onChange={(e) => setBio(e.target.value)}
+                    className="form-control border-none"
+                    placeholder='Enter bio'
                     />
                 </td>
                 <td>
@@ -108,6 +120,8 @@ const AddDoctor = () => {
                     type="text"
                     value={specialization}
                     onChange={(e) => setSpecialization(e.target.value)}
+                    className="form-control border-none"
+                    placeholder='Enter specialization'
                     />
                 </td>
                 <td>
@@ -115,12 +129,14 @@ const AddDoctor = () => {
                     type="text"
                     value={photo}
                     onChange={(e) => setPhoto(e.target.value)}
+                    className="form-control border-none"
+                    placeholder='Enter photo url'
                     />
                 </td>
                 </tr>
             </tbody>
             </table>
-            <button type="button" className="btn btn-primary" onClick={onClickHandle}
+            <button type="button" className="btn btn-primary w-100"  onClick={onClickHandle}
             >
             Add Doctor
             </button>
