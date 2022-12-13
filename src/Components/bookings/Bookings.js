8620aba3c71dd93832b0fd1bb89ba9ed79f8d0cf
Anas Abubakar <anasabubakar2015@gmail.com>
2022-12-13 13:17:24 +0100

    Add styling to bookings

--- src/Components/bookings/Bookings.js
+++ src/Components/bookings/Bookings.js
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchBookings } from '../../Redux/bookings/bookings';
 // import { cancelBooking } from '../../Redux/bookings/bookings';
 import { Navigate } from 'react-router-dom';
+import '../../Styles/bookings.css'
 
 const Bookings = () => {
   const dispatch = useDispatch();
@@ -23,29 +24,48 @@ else {
   const displayBookings = bookings.length !== 0 ? (
     bookings.map((booking) => (
       <div className="reserve-dev">
-        <div key={booking.id}>
-        
-            <strong>Doctor Name</strong>
-  
-            <h3>first_name : {booking.doctor.first_name}</h3>
-            <h3>last_name: {booking.doctor.last_name}</h3>
-              {console.log(booking.doctor.first_name)} 
-          <h4>City: {booking.city}</h4>
-          <p>Date: {booking.date}</p>
-          <p>Time: {booking.time}</p>
-          <p>Disease Symptoms: {booking.disease_desc}</p>
-          
+        <div key={booking.id} className="booking">
+
+          <div className="booking-det">
+            <h2>
+              {booking.doctor.first_name}
+              {' '}
+              {booking.doctor.last_name}
+            </h2>
+            <table className="table">
+              <tr>
+                <td>City</td>
+                <td>{booking.city}</td>
+              </tr>
+              <tr>
+                <td>Contact Number</td>
+                <td>{booking.doctor.phone}</td>
+              </tr>
+              <tr>
+                <td>Date</td>
+                <td>{booking.date}</td>
+              </tr>
+              <tr>
+                <td>Time</td>
+                <td>{booking.time}</td>
+              </tr>
+              <tr>
+                <td>Disease Symptoms</td>
+                <td>{booking.disease_desc}</td>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
     ))
   ) : (
     <div>
-      <h4 className="reservations">No bookings.</h4>
+      <h3 className="reservations">No bookings.</h3>
     </div>
   );
 
   return (
-    <div className="container">
+    <div className="booking-container">
       <h1>BOOKINGS LIST</h1>
       {displayBookings }
       
